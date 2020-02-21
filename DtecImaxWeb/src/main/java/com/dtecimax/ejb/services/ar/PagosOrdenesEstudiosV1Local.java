5a926a49e91c6e7a4ca713b34c8b4bb47e9b9ebf
Gabino <gabino.hernandez@dcprosoft.com.mx>
2020-02-21 12:33:00 -0600

    Commit21022020 from Gabo06022020

--- DtecImaxWeb/src/main/java/com/dtecimax/ejb/services/ar/PagosOrdenesEstudiosV1Local.java
+++ DtecImaxWeb/src/main/java/com/dtecimax/ejb/services/ar/PagosOrdenesEstudiosV1Local.java
@@ -1,10 +1,12 @@
 package com.dtecimax.ejb.services.ar;
 
+import java.math.BigDecimal;
 import java.sql.Date;
 import java.util.List;
 
 import javax.ejb.Local;
 
+import com.dtecimax.ejb.model.ar.AdmonPagOrdEstud;
 import com.dtecimax.jpa.dto.ar.PagosOrdenesEstudiosV1Dto;
 
 @Local
@@ -16,5 +18,16 @@ public interface PagosOrdenesEstudiosV1Local {
 											            ,Date pFechaDesde
 											            ,Date pFechaHasta
 											            );
-	
+	public List<AdmonPagOrdEstud> findPagosConTarjConFact(Date pFechaDesde
+                                                         ,Date pFechaHasta);
+	public List<AdmonPagOrdEstud> findPagosConTarjSinFact(Date pFechaDesde
+	                                                     ,Date pFechaHasta);
+	public List<AdmonPagOrdEstud> findPagosConEfectivo(Date pFechaDesde
+	                                                  ,Date pFechaHasta);
+	public List<AdmonPagOrdEstud> findPagosConCLIP(Date pFechaDesde
+	                                              ,Date pFechaHasta);
+	public BigDecimal sumaPagosConTarjConFact(Date pFecha); 
+	public BigDecimal sumaPagosConTarjSinFact(Date pFecha); 
+	public BigDecimal sumaPagosConEfectivo(Date pFecha); 
+	public BigDecimal sumaPagosConCLIP(Date pFecha); 
 }
--- DtecImaxWeb/src/main/java/com/dtecimax/jpa/dao/ar/PagosOrdenesEstudiosV1Dao.java
+++ DtecImaxWeb/src/main/java/com/dtecimax/jpa/dao/ar/PagosOrdenesEstudiosV1Dao.java
@@ -1,5 +1,6 @@
 package com.dtecimax.jpa.dao.ar;
 
+import java.math.BigDecimal;
 import java.sql.Date;
 import java.util.List;
 
@@ -13,5 +14,18 @@ public interface PagosOrdenesEstudiosV1Dao {
 			                                            ,Date pFechaDesde
 			                                            ,Date pFechaHasta
 			                                            );
+	public List<Object> findPagosConTarjConFact(Date pFechaDesde
+                                               ,Date pFechaHasta);
+	public List<Object> findPagosConTarjSinFact(Date pFechaDesde
+                                               ,Date pFechaHasta);
+	public List<Object> findPagosConEfectivo(Date pFechaDesde
+                                            ,Date pFechaHasta);
+	public List<Object> findPagosConCLIP(Date pFechaDesde
+                                         ,Date pFechaHasta);
+	
+	public BigDecimal sumaPagosConTarjConFact(Date pFecha); 
+	public BigDecimal sumaPagosConTarjSinFact(Date pFecha); 
+	public BigDecimal sumaPagosConEfectivo(Date pFecha); 
+	public BigDecimal sumaPagosConCLIP(Date pFecha); 
 	
 }
